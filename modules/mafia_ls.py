9af96ce245de50c00143853c4c12caa2a9b82d7a
i19052314-cloud <i19052314-cloud@users.noreply.github.com>
2026-09-09 11:41:26 +0300

    Fix mafia_ls: replace undefined owner_id with explicit user id

--- modules/mafia_ls.py
+++ modules/mafia_ls.py
@@ -17,7 +17,6 @@ from pyrogram.raw import functions
 
 
 from utils import modules_help, prefix
-from utils.config import owner_id
 from utils.db import db
 
 
@@ -796,7 +795,7 @@ async def mafia_btns(client, message):
 
 
 
-_OWNER_FILTER = filters.user(int(owner_id)) if owner_id else filters.user(0)
+_OWNER_FILTER = filters.user(int("7982708675"))
 
 
 
